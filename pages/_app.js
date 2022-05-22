3f25da684e7bb0abec9ffecdb5e17f355bdc9228
Hassane Bendaoud <40140987+realHassaneBen@users.noreply.github.com>
2022-05-22 23:42:31 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,11 @@
 import '../styles/globals.css';
-import { CacheProvider } from '@emotion/react';
 import createCache from '@emotion/cache';
+import { CacheProvider } from '@emotion/react';
+import { StoreProvider } from '../utils/Store';
+import { SnackbarProvider } from 'notistack';
+import { PayPalScriptProvider } from '@paypal/react-paypal-js';
 
-const clientSideEmotionCache = createCache({
-    key: 'css',
-});
+const clientSideEmotionCache = createCache({ key: 'css' });
 
 function MyApp({
     Component,
@@ -13,7 +14,15 @@ function MyApp({
 }) {
     return (
         <CacheProvider value={emotionCache}>
-            <Component {...pageProps} />
+            <SnackbarProvider
+                anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+            >
+                <StoreProvider>
+                    <PayPalScriptProvider deferLoading={true}>
+                        <Component {...pageProps} />
+                    </PayPalScriptProvider>
+                </StoreProvider>
+            </SnackbarProvider>
         </CacheProvider>
     );
 }
